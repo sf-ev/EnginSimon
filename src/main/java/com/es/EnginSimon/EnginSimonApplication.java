e1c1b096fa7e49dd6562d76ca6d0e46386f1d07d
Simon Frank <s.frank@intra.evia.de>
2021-09-21 13:22:37 +0200

    Initial commit

---
+++ src/main/java/com/es/EnginSimon/EnginSimonApplication.java
@@ -0,0 +1,13 @@
+package com.es.EnginSimon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnginSimonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnginSimonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/es/EnginSimon/EnginSimonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.es.EnginSimon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnginSimonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
